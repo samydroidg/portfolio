50f1027f201979e51a62aaf935653bed031b543b
Gourav Ojha <166712869+samydroidg@users.noreply.github.com>
2025-06-08 13:51:32 +0530

    change 1
    added themes and a cursor trail with some sound effets

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 const faders = document.querySelectorAll('.fade-in');
+const themeToggle = document.getElementById('theme-toggle');
+const buttons = document.querySelectorAll('.btn');
+const hoverSound = new Audio('sound/button.mp3');
+const email = document.getElementById('email');
 
 document.addEventListener("DOMContentLoaded", function(){
     const heading = document.querySelector('h1');
@@ -29,3 +33,28 @@ function showOnScroll() {
 
 window.addEventListener('scroll', showOnScroll);
 window.addEventListener('load', showOnScroll);
+
+themeToggle.addEventListener('click', () => {
+    document.body.classList.toggle('light-mode');
+});
+
+buttons.forEach(btn => {
+    btn.addEventListener('mouseenter', () => {
+        hoverSound.currentTime = 0;
+        hoverSound.play();
+    });
+});
+
+email.addEventListener('click', () => {
+    navigator.clipboard.writeText("gouravojha.2005@gmail.com").then(() => alert("Email copied to clipboard!")).catch(()=> alert("Failed to copy email!"));
+});
+
+document.addEventListener('mousemove', e => {
+    const dot = document.createElement('div');
+    dot.classList.add('cursor-dot');
+    dot.style.left = `${e.clientX}px`;
+    dot.style.top = `${e.clientY}px`;
+    document.body.appendChild(dot);
+
+    setTimeout(() => dot.remove(), 200);
+});
